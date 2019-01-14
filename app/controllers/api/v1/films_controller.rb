e6516650f51d87ae62a645ba085d7c42367ffd0c
Jamie Macey <jamie.git@tracefunc.com>
2019-01-14 14:51:01 -0800

    stub some data in

--- app/controllers/api/v1/films_controller.rb
+++ app/controllers/api/v1/films_controller.rb
@@ -1,5 +1,11 @@
 class Api::V1::FilmsController < Api::V1::BaseController
   def index
-    respond_with []
+    stub = {
+      title: "Bumblebee",
+      duration: 113,
+      times2d: ['4:15'],
+      times3d: ['7:10', '9:50'],
+    }
+    respond_with [stub]
   end
 end
