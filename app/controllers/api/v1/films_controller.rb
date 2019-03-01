0cd91992aa54f00cbb19f31e4d59686b0135c6b4
Jamie Macey <jamie.git@tracefunc.com>
2019-03-01 10:37:15 -0800

    change declaration style for reloader support

--- app/controllers/api/v1/films_controller.rb
+++ app/controllers/api/v1/films_controller.rb
@@ -1,19 +1,23 @@
-require 'cinema_clock'
+# frozen_string_literal: true
 
-class Api::V1::FilmsController < Api::V1::BaseController
-  def index
-    films = []
-    if (url = params['source'])
-      films = CinemaClock.new(url).films
-    else
-      # Stub
-      films << {
-        title:    'No Show',
-        theatre:  'No Theatre',
-        duration: 120,
-        showings: [],
-      }
+module Api
+  module V1
+    class FilmsController < Api::V1::BaseController
+      def index
+        films = []
+        if (url = params['source'])
+          films = CinemaClock.films_at(url)
+        else
+          # Stub
+          films << {
+            title: 'No Show',
+            theatre: 'No Theatre',
+            duration: 120,
+            showings: []
+          }
+        end
+        render json: films.to_json
+      end
     end
-    render json: films.to_json
   end
 end
